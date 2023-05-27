6be90b561cb3fac56d4bf31cf4881057ca7da1cd
Verga Tandika <64019419+vergatan10@users.noreply.github.com>
2023-05-27 18:29:19 +0700

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import "@styles/globals.css";
+
+import Nav from "@components/nav";
+import Provider from "@components/provider";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
